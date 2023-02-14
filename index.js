00751b720d23f35482f4dde091f83743bee706e6
aldwiputra <aldiansyhdp@gmail.com>
2023-02-14 09:00:55 +0700

    add assets, header html, and global css

---
+++ index.js
@@ -0,0 +1,11 @@
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+hamburgerMenu.addEventListener('click', menuClickHandler);
+
+function menuClickHandler(event) {
+   addOrRemoveClass(hamburgerMenu, 'active');
+}
+
+function addOrRemoveClass(element, className, method = 'toggle') {
+   element.classList[method](className);
+}
